55dff5d866dc39b4037495cd031bac94a823c3c9
Denasia <dfurniss@ymail.com>
2023-02-06 14:52:48 -0500

    open and close cart, cart icons

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { AiFillCloseSquare } from "react-icons/ai";
+import "./Cart.css";
 
 export default function Cart(){
+    const closeCart = () => {
+        const cart = document.getElementById("Cart");
+        cart.style.display = "none";
+    }
+
     return (
-        <div>
-            CART
+        <div id="Cart">
+            <AiFillCloseSquare size={30} color="3f3f3f" onClick={closeCart} cursor="pointer"/>
         </div>
     )
-}
+};
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import './Nav.css'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import './Nav.css';
 
 export default function Nav(){
+    const openCart = () => {
+        const cartDiv = document.getElementById("Cart");
+        cartDiv.style.display = "block";
+    }
+
     return (
         <nav id="Nav">
           <h1>DDD</h1>
-          <div id="cart"><AiOutlineShoppingCart size={30}/></div>
+          <div id="cart"><AiOutlineShoppingCart size={30} onClick={openCart} cursor="pointer"/></div>
         </nav>
     )
-}
+};
