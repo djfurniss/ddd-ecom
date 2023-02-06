951f16bfdb8fe7c85e2f0f52550862e9d5d42dd4
Denasia <dfurniss@ymail.com>
2023-02-06 14:54:33 -0500

    wrapped info and add to cart icon in a div.row

--- src/products/Product.js
+++ src/products/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Product.css"
+import { BsCartPlus } from "react-icons/bs"
+import "./Product.css";
 
 export default function Product(){
     // TODO: props = img_src, name, price
@@ -8,8 +9,16 @@ export default function Product(){
             <div>
                 <img src="https://images.unsplash.com/photo-1634986666676-ec8fd927c23d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"/>
             </div>
-            <h3>Name</h3>
-            <p>Price</p>
+            <div className="row between">
+                <div>
+                    <h3>Name</h3>
+                    <p>Price</p>
+                </div>
+                <div id="atc">
+                    {/* add to cart icon */}
+                    <BsCartPlus size={25} color="3f3f3f"/>
+                </div>
+            </div>
         </div>
     )
 }
