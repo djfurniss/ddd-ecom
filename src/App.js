d10c1f43ff909a55aafdaf0de7141888e67b8d21
Denasia <dfurniss@ymail.com>
2023-02-06 13:40:04 -0500

    corrected spelling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "./nav/Nav";
-import Produts from "./products/Products";
+import Products from "./products/Products";
 import Cart from "./cart/Cart";
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
       {/* nav bar */} {/* WITH cart icon that will expand out */}
       <Nav/>
       {/* products */}
-      {/* <Produts/> */}
+      {/* <Products/> */}
       {/* cart.. display value toggles when cart icon is clicked or is closed*/}
       {/* <Cart/> */}
     </div>
--- src/products/Products.js
+++ src/products/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Produts(){
+export default function Products(){
     return(
         <div>
             "PRODUCTS"
