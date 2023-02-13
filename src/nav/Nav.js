1f67b78276f23103808db2105b6f0239a1be5b7b
Denasia <dfurniss@ymail.com>
2023-02-13 16:14:30 -0500

    todo comment

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { AiOutlineShopping } from 'react-icons/ai';
 import './Nav.css';
 
-export default function Nav(){
-    const openCart = () => {
-        const cartDiv = document.getElementById("Cart");
-        cartDiv.style.display = "block";
-    }
-
+export default function Nav({ setCartVisible }){
     return (
         <nav id="Nav">
           <h1>DDD</h1>
-          <div id="cart"><AiOutlineShoppingCart size={30} onClick={openCart} cursor="pointer"/></div>
+          <div id="cart"><AiOutlineShopping size={30} onClick={()=>setCartVisible(true)} cursor="pointer"/></div>
         </nav>
     )
 };
+
+// TODO: have badge for how many items in cart
